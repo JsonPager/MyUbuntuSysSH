dcc100bcd01cd0d75d655da6f5bac3277d3b3698
JsonPager <144886165+JsonPager@users.noreply.github.com>
2024-08-29 12:27:45 +0800

    Update sysenv.sh

--- sysenv.sh
+++ sysenv.sh
@@ -121,7 +121,7 @@ function checkdockerandnet() {
         # echo "docker未安装或者无法启动"
         return 1
     fi
-    # 验证网络是否已经存在（这个地方有点奇怪，必须先赋值再判断）
+    # 验证网络是否已经存在
     local checknetresult=$(check_network)
     if [[ $checknetresult -eq 1 ]]; then
         #echo "不存在mynet网络"
@@ -134,9 +134,9 @@ function checkdockerandnet() {
 # 验证容器存在情况（输入参数，容器名称）
 function testcontainer() {
     if docker ps -a --filter "NAME=$1" | grep -q "$1"; then
-        return 1
+        echo "1"
     else
-        return 0
+        echo "0"
     fi
 }
 
@@ -199,9 +199,8 @@ while true; do
     10)
         if [[ $checkdockerandnet -eq 0 ]]; then
             read -p "请输入需要卸载的容器名称: " uncontainername
-            # 赋值操作=号左右两边不能有空格
-            checkcontainerresult=$(testcontainer $uncontainername)
-            if [[ $checkcontainerresult -eq 1 ]]; then
+            checkcontainerresult="$(testcontainer $uncontainername)"         
+            if [[ "$checkcontainerresult" == "1" ]]; then
                 echo "找到了需要卸载的容器了"
                 getCONTAINER_ID=$(docker ps -a --format "{{.ID}}" --filter "name=$uncontainername")
                 delvolumes=$(docker inspect --format='{{json .Mounts}}' "$getCONTAINER_ID")
